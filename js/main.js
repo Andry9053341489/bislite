28f20a4e6fa7f44b50a95875973409fd1cfecc9c
Andry9053341489 <reg9053341489@yandex.ru>
2020-12-23 02:12:03 +0400

    end

--- js/main.js
+++ js/main.js
@@ -11,10 +11,34 @@ $(function () {
 		slidesToScroll: 1,
 		prevArrow: '<button class="slick-prev slick-arrow"><img src="/img/works-slider/works-slider-arrow-left.svg" alt=""></button>',
 		nextArrow: '<button class="slick-next slick-arrow"><img src="/img/works-slider/works-slider-arrow-right.svg" alt=""></button>',
+		responsive: [{
+				breakpoint: 961,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 731,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 501,
+				settings: {
+					slidesToShow: 1
+				}
+			},
+		]
 	});
 
-	$('.header__burger-btn').on('click', function(){
-		$('.header__menu-items').slideToggle();
+	$('.header__burger-btn').on('click', function () {
+		$('.header__menu').slideToggle();
 	});
+
+	$('.drop-down').on('click', function () {
+		$(this).children('.drop-down__list').slideToggle();
+	});
+
 
 });
